6695101a0df6b2c69470b187396b6b6dedcbf25f
hajerabdelfatah153-alt <hajerabdelfatah153@gmail.com>
2026-09-02 19:50:56 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ export default function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 1000);
+    }, 2000);
 
     return () => clearTimeout(timer);
   }, []);
